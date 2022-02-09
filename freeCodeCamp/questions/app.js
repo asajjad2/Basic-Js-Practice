756264fc948be9b57f825377d34f8dc91a804ed1
Areeb Sajjad <areebsajjad21@gmail.com>
2022-02-09 18:56:31 +0500

    completed questions task

--- freeCodeCamp/questions/app.js
+++ freeCodeCamp/questions/app.js
@@ -1,3 +1,27 @@
-//using selectors inside the element
+
 // traversing the dom
 
+
+    // const btns = document.querySelectorAll(".question-btn");
+    // btns.forEach(function(btn){
+    //     btn.addEventListener('click',function(e){
+    //         const grandp = e.currentTarget.parentElement.parentElement;
+    //         // console.log(grandp);
+    //         grandp.classList.toggle('show-text');
+    //     })
+    // })
+
+
+//using selectors inside the element
+
+    const questions = document.querySelectorAll(".question");
+    questions.forEach(function(q){
+        btn = q.querySelector('.question-btn');
+        btn.addEventListener('click',function(){
+            questions.forEach(function(question){
+                question.classList.remove('show-text');
+            })
+            q.classList.toggle('show-text');
+            
+        })
+    })
